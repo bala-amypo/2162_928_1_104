0a0429b95585c935bd6333ba8151aedd7791fd23
bala-amypo <bala@amypo.in>
2025-12-22 10:26:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/IssuedDeviceRecordController.java
+++ src/main/java/com/example/demo/controller/IssuedDeviceRecordController.java
@@ -1,3 +1,17 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.entity.IssuedDeviceRecord;
+import com.example.demo.service.IssuedDeviceRecordService;
+
 @RestController
 @RequestMapping("/api/issued-devices")
 public class IssuedDeviceRecordController {
@@ -9,7 +23,7 @@ public class IssuedDeviceRecordController {
     }
 
     @PostMapping
-    public IssuedDeviceRecord issue(@RequestBody IssuedDeviceRecord record) {
+    public IssuedDeviceRecord issueDevice(@RequestBody IssuedDeviceRecord record) {
         return service.issueDevice(record);
     }
 
