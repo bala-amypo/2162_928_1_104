8de0e9e874b635a2e6f573577b524845afbde04a
bala-amypo <bala@amypo.in>
2025-12-25 17:38:32 +0000

    User commit

--- src/main/java/com/example/demo/service/IssuedDeviceRecordService.java
+++ src/main/java/com/example/demo/service/IssuedDeviceRecordService.java
@@ -1,11 +1,16 @@
 package com.example.demo.service;
 
 import com.example.demo.model.IssuedDeviceRecord;
+
 import java.util.List;
 
 public interface IssuedDeviceRecordService {
 
     IssuedDeviceRecord issueDevice(IssuedDeviceRecord record);
+
+    IssuedDeviceRecord returnDevice(Long recordId);
+
+    List<IssuedDeviceRecord> getAll();
 
     List<IssuedDeviceRecord> getIssuedDevicesByEmployee(Long employeeId);
 }
