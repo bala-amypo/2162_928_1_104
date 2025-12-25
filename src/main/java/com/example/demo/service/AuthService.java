8617db03382ce22f54b5c1ba99adddef9552baf0
bala-amypo <bala@amypo.in>
2025-12-25 08:20:01 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -3,5 +3,8 @@ package com.example.demo.service;
 import com.example.demo.model.UserAccount;
 
 public interface AuthService {
-    String login(UserAccount userAccount);
+
+    UserAccount register(UserAccount user);
+
+    String login(UserAccount user);
 }
